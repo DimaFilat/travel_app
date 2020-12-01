9fc4214e4ba3da89f1a1be9bf7d66136cf6348f3
Dmitriy Filtachev <o6jlako@mail.ru>
2020-12-01 05:35:58 +0300

    initial

--- App.tsx
+++ App.tsx
@@ -1,14 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper'
+import { Theme } from 'react-native-paper/lib/typescript/src/types'
+
+const theme: Theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#3498db',
+    accent: '#f1c40f'
+  }
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <PaperProvider theme={theme}>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style='auto' />
+      </View>
+    </PaperProvider>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -16,6 +29,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
